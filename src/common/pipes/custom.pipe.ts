77cde01488dfdfc08ee960126202013b0fcf0754
greatorangejuice <bypavelsnigirev@gmail.com>
2021-01-04 12:25:42 +0300

    test

--- src/common/pipes/custom.pipe.ts
+++ src/common/pipes/custom.pipe.ts
@@ -5,7 +5,7 @@ export class CustomPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata): any {
     console.log(`CUSTOM PIPE value: ${JSON.stringify(value)}`);
     if (metadata) {
-      console.log(`CUSTOM PIPE metadata: ${JSON.stringify(metadata)}`);
+      console.log(`CUSTOM PIPE metadata: ${JSON.stringify(metadata)}!!!`);
     }
     return undefined;
   }
